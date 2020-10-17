f2abf714c927bc7e0a76d72e1b69715b2ee93cbd
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-17 20:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 function appendKitten(name){
   var newArray = kittens.slice();
-  // or ES6 way
-  // var newArray = [...kittens];
   newArray.push(name)
   return newArray
+}
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
 }
