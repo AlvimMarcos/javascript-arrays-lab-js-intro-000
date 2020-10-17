1f4e802f755b5215c3d63003872a2556575a5778
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-17 21:19:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function prependKitten(name) {
   return[ name, ...kittens]
 }
 
-function removeLastKitten() {
-  return kittens.slice(0,kittens.length-1)
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
 }
